32e40fbe8fc5c676a3ac96625f6cf2d5d64fdda6
TOBU66601 <jr-e-e653-kenshiro@outlook.com>
2018-03-06 14:31:22 +0900

    ファイル挿入完了
    必要ファイルの挿入完了。

---
+++ Kakokiji.js
@@ -0,0 +1,48 @@
+﻿//最新記事リスト
+function kako() {
+    var html = "";
+    //html
+    html += '<div id="blognav">';
+    html += '<h3>';
+    html += '過去の記事';
+    html += '</h3>';
+
+    html += '<p>';
+    html += '2018/2/2<br/>';
+    html += '<a href="/blog/article/20180202.html">';
+    html += '●　自作環境の道：パーツ購入';
+    html += '</a>';
+    html += '</p>';
+
+    html += '<p>';
+    html += '2018/1/28<br/>';
+    html += '<a href="/blog/article/20180128.html">';
+    html += '●　1月27日　.NET Fringe Japan 2018 New Year Party　イベントレポート';
+    html += '</a>';
+    html += '</p>';
+
+    html += '<p>';
+    html += '2018/1/24<br/>';
+    html += '<a href="/blog/article/20180124.html">';
+    html += '●　1月20日　JXUGC#24 春のApp Centerまつり　イベントレポート';
+    html += '</a>';
+    html += '</p>';
+
+
+
+
+
+
+    html+='<h3><p><a href="/blog/list.html">ブログ記事一覧</a></p></h3>' //位置の再編集
+    html += '<div><p>';
+    html += '<a href="/blog/article/nipponlinetime.html">';
+    html += '●　BVE日本線　ソースファイル';
+    html += '</a>';
+
+    html += '</p></div>';
+
+    html += '</div>';
+
+    document.write(html);
+    document.write('<link rel="stylesheet" href="css/kiji.css" type="text/css" />');
+}
---
+++ indexload.js
@@ -0,0 +1,33 @@
+﻿var load_html_and_insert = function (html_url, insert_info_arr) {
+    $.ajax(html_url, {
+        timeout: 1000,
+        datatype: 'html'
+    }).then(function (data) {
+        var out_html = $($.parseHTML(data));//parse1
+        var i;
+        for (i = 0; i < insert_info_arr.length; ++i) {
+            $("#" + insert_info_arr[i][1]).empty().append(out_html.filter("#" + insert_info_arr[i][0])[0].innerHTML);//insert
+        }
+    }, function (jqXHR, textStatus) {
+        if (textStatus !== "success") {
+            var txt = "<p>textStatus:" + textStatus + "</p>" +
+                "<p>status:" + jqXHR.status + "</p>" +
+                "<p>responseText : </p><div>" + jqXHR.responseText +
+                "</div>";
+            $('#title').html(txt);
+            $('#date').html(txt);
+            $('#kiji').html(txt);
+
+
+        }
+    });
+};
+
+
+
+
+
+
+
+
+
